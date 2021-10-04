10c97bdca2bcabbbfc21eedbf071835742f41161
ijsathi <ijsathi2001rs@gmail.com>
2021-10-04 21:16:02 +0600

    All Done

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Too many course available.
 
 #### Visit our Live site
 
-Live site link: 
+Live site link: https://ij-educational-site.netlify.app/
 ### Take a time and visit my education center
 ### Take my cordial love
 ### ALLAH is trustworthy
--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Banner from '../Banner/Banner';
 import EducationFooter from '../EducationFooter/EducationFooter';
-import MiddlePart from '../MiddlePart/MiddlePart';
 
 const Service = () => {
     return (
