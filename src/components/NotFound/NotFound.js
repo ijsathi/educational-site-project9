9b2246008de27a5f3dd9e231a2c72bb25db8d8fa
ijsathi <ijsathi2001rs@gmail.com>
2021-10-05 20:41:29 +0600

    OK!

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img from '../../images/404.gif'
 
 const NotFound = () => {
     return (
         <div className='not-found'>
-            <img src={img} alt="" />
+            <div>
+                 <img src={img} alt="" />
+            </div>
+           <Link to="/home"><button>Back to Home</button></Link>
         </div>
     );
 };
